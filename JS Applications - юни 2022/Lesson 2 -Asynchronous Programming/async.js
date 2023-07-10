f3826c83db889914d6858316fa8d841259afc1c3
AngelKasabov <ankasabov142@gmail.com>
2023-07-10 19:07:17 +0300

    Start of the new git backup of my learning

---
+++ JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/async.js
@@ -0,0 +1,7 @@
+let e=document.getElementById('button')
+e.addEventListener("click", myFunction);
+
+function myFunction(){
+    const promise=fetch("https://swapi.dev/api/people/1");
+    console.log(promise);
+}
