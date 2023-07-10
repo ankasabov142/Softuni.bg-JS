bcb516e0dfd3b6f55b59e55380e339a536534706
AngelKasabov <ankasabov142@gmail.com>
2023-07-10 21:20:17 +0300

    .

--- JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/async.js
+++ JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/async.js
@@ -4,4 +4,5 @@ e.addEventListener("click", myFunction);
 function myFunction(){
     const promise=fetch("https://swapi.dev/api/people/1");
     console.log(promise);
+    promise.then(res => console.log(res))
 }
