fe28bf3ba392007b7a90d4a17f2f0f8b1325d2c5
AngelKasabov <ankasabov142@gmail.com>
2023-07-12 18:24:27 +0300

    solved a problem

--- JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/ex2 lab-github repos/app.js
+++ JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/ex2 lab-github repos/app.js
@@ -1,3 +1,34 @@
 function loadRepos() {
-	console.log("TODO...");
+  //read input field
+  const user = document.getElementById("username").value;
+  const list = document.getElementById("repos");
+  list.innerHTML = "";
+  const url = `https://api.github.com/users/${user}/repos`;
+  try {
+    async function readData(url) {
+      const res = await fetch(url);
+      const data = await res.json();
+	  if(res.ok!=true){
+throw new Error();
+	  }
+      for (let repo of data) {
+        let li = document.createElement("li");
+        let repoName = repo.name;
+        let repoUrl = repo.html_url;
+        let a = document.createElement("a");
+        let link = document.createTextNode(`${user}/${repoName}`);
+        a.appendChild(link);
+        a.title = `${user}/${repoName}`;
+        a.href = repoUrl;
+        li.appendChild(a);
+        list.appendChild(li);
+      }
+    }
+	readData(url);
+  } catch {
+    alert("404 “Not Found”");
+  }
+
+  
+  //display response data
 }
