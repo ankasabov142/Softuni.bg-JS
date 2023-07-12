b8023f2b54592194a1ebe346af9ebf25e49a13de
AngelKasabov <ankasabov142@gmail.com>
2023-07-12 17:21:00 +0300

    up

---
+++ JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/demo.js
@@ -0,0 +1,11 @@
+function start(){
+
+
+    a=>
+    setTimeout(a,2000,5);
+for(i=0;i<=100;i++){
+    console.log(i);
+}
+
+}
+start()
--- JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/fetchDemo.js
+++ JS Applications - юни 2022/Lesson 2 -Asynchronous Programming/fetchDemo.js
@@ -2,7 +2,7 @@ let e=document.getElementById('button');
 e.addEventListener('click',onclick)
 
 function onclick() {
-  fetch("https://swapi.dev/api/people")
+  fetch("https://restcountries.com/v3.1/name/bulgaria")
     .then(res => res.json())
     .then(data => document.getElementById("text").value= JSON.stringify(data,null,2))
 }
