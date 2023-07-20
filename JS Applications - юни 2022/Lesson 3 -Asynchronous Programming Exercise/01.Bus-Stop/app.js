f7bb36b7bcd0ffd1f9618468acfedd99138b8236
Angel Kasabov <ankasabov142@gmail.com>
2023-07-20 18:08:42 +0300

    fixed errors

--- JS Applications - юни 2022/Lesson 3 -Asynchronous Programming Exercise/01.Bus-Stop/app.js
+++ JS Applications - юни 2022/Lesson 3 -Asynchronous Programming Exercise/01.Bus-Stop/app.js
@@ -1,28 +1,25 @@
 function getInfo() {
-    let baseUrl = "http://localhost:3030/jsonstore/bus/businfo";
-    let inputElement = document.getElementById('stopId');
-    let ulElements = document.getElementById('buses');
-    let divElement = document.getElementById("stopName");
+  let baseUrl = "http://localhost:3030/jsonstore/bus/businfo";
+  let inputElement = document.getElementById("stopId");
+  let ulElements = document.getElementById("buses");
+  let divElement = document.getElementById("stopName");
 
-    fetch(`${baseUrl}/${inputElement.value}`)
-    .then(response => response.json())
-    .then(data => {
-        let buses=data.buses;
-        let name=data.name ;
+  fetch(`${baseUrl}/${inputElement.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let buses = data.buses;
+      let name = data.name;
 
-        divElement.textContent = name;
-        ulElements.innerHTML = '';
-        Object.keys(buses).forEach(bus => {
-            let liElement = document.createElement('li');
-            liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
-            ulElements.appendChild(liElement);
-        })
+      divElement.textContent = name;
+      ulElements.innerHTML = "";
+      Object.keys(buses).forEach((bus) => {
+        let liElement = document.createElement("li");
+        liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+        ulElements.appendChild(liElement);
+      });
     })
-.catch(error => {
-    divElement.textContent = "Error"});
-    ulElements.innerHTML= '';
-
-
-
-
+    .catch((error) => {
+      divElement.textContent = "Error";
+    });
+  ulElements.innerHTML = "";
 }
